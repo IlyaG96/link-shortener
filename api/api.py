94b4dd6c1b5c11e7e124d63266a2dae6efcc3cfe
IlyaG96 <ilyag_96@icloud.com>
2022-03-01 21:59:50 +1100

    remove unused code

--- api/api.py
+++ api/api.py
@@ -51,19 +51,5 @@ def get_short_link():
         return "error"
     return short_link
 
-
-"""@app.route('/<path:path>', methods=["GET", "POST", "DELETE"])
-def proxy(path):
-    global SITE_NAME
-    all_args = request.args.to_dict()
-    if request.method == 'GET':
-        url = urljoin(SITE_NAME, path)
-        resp = requests.get(url=url, params=all_args)
-        resp.raise_for_status()
-        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers,)
-        return response"""
-
 if __name__ == '__main__':
     app.run()
