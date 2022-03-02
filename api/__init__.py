8b7010c11be59190d31b68eccb806cd84d8a5013
IlyaG96 <ilyag_96@icloud.com>
2022-03-02 17:38:33 +1100

    add custom link

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,5 @@
 from flask_redis import Redis
-from flask import Flask
+from flask import Flask, url_for, current_app
 from environs import Env
 
 app = Flask(__name__)
@@ -9,6 +9,7 @@ env.read_env()
 redis_host = env.str('REDIS_HOST')
 redis_port = env.str('REDIS_PORT')
 redis_password = env.str('REDIS_PASSWORD')
+app.config['SERVER_NAME'] = '127.0.0.1:5000'
 app.config['REDIS_HOST'] = redis_host
 app.config['REDIS_PORT'] = redis_port
 app.config['REDIS_PASSWORD'] = redis_password
--- api/falskapp.py
+++ api/falskapp.py
@@ -1,13 +1,41 @@
-from flask import request, redirect, jsonify
+import requests
+from flask import request, redirect, jsonify, send_from_directory
 from hashlib import sha256
 from api import app, redis
-from enum import Enum
+import os
 
 
-class Responses(Enum):
+class Responses:
     NO_SUCH_SHORT_LINK = {'message': 'no such short link in database'}
     NOT_QUERY_PARAMS = {'message': 'have not got query params'}
     INCORRECT_QUERY_PARAMS = {'message': 'incorrect query params'}
+    WRONG_QUERY_PARAMS = {'message': 'incorrect query params'}
+    INCORRECT_LINK = {'message': 'incorrect link'}
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+@app.route('/<link_id>')
+def redirect_to_other_domain(link_id):
+    url = redis.hget(link_id, link_id).decode()
+    if not url:
+        return jsonify(Responses.NO_SUCH_SHORT_LINK)
+    return redirect(url)
+
+
+def check_link(full_link):
+
+    try:
+        response = requests.get(full_link, timeout=2)
+        response.raise_for_status()
+        return response.ok or response.is_redirect
+
+    except requests.RequestException:
+        return False
 
 
 def write_custom_link_db(full_link, link_name):
@@ -17,23 +45,14 @@ def write_custom_link_db(full_link, link_name):
     redis.hset(link_id, link_id, full_link)
     redis.hset(full_link, full_link, link_id)
 
-    return link_id
-
 
 def write_link_db(full_link):
+
     link_id = sha256(full_link.encode()).hexdigest()[:8]
     redis.hset(link_id, link_id, full_link)
     redis.hset(full_link, full_link, link_id)
 
     return link_id
-
-
-@app.route('/<link_id>', methods=['GET'])
-def redirect_to_other_domain(link_id):
-    url = redis.hget(link_id, link_id).decode()
-    if not url:
-        return jsonify(Responses.NO_SUCH_SHORT_LINK)
-    return redirect(url)
 
 
 @app.route('/api/custom', methods=['GET'])
@@ -42,11 +61,22 @@ def make_custom_link():
         return jsonify(Responses.NOT_QUERY_PARAMS)
 
     query_params = request.args.to_dict()
-    full_link = query_params.get('link')
-    link_name = query_params.get('name')
-    link_id = write_custom_link_db(full_link, link_name)
 
-    return f'127.0.0.1:5000/{link_id}'
+    full_link = query_params.get('link')
+
+    link_name = query_params.get('name')
+
+    if not (full_link or link_name):
+        return jsonify(Responses.WRONG_QUERY_PARAMS)
+
+    check_link(full_link)
+
+    if not check_link(full_link):
+        return jsonify(Responses.INCORRECT_LINK)
+
+    write_custom_link_db(full_link, link_name)
+
+    return jsonify({'message': f'127.0.0.1:5000/{link_name}'})
 
 
 @app.route('/api/make-short', methods=['GET'])
@@ -56,6 +86,13 @@ def make_short_link():
 
     query_params = request.args.to_dict()
     full_link = query_params.get('link')
+
+    if not full_link:
+        return jsonify(Responses.WRONG_QUERY_PARAMS)
+
+    if not check_link(full_link):
+        return jsonify(Responses.INCORRECT_LINK)
+
     link_id = write_link_db(full_link)
 
     return f'127.0.0.1:5000/{link_id}'
@@ -67,15 +104,21 @@ def get_short_link():
         return jsonify(Responses.NOT_QUERY_PARAMS)
     query_params = request.args.to_dict()
     full_link = query_params.get('link')
+
+    if not full_link:
+        return jsonify(Responses.WRONG_QUERY_PARAMS)
+
     link_id = redis.hget(full_link, full_link).decode()
 
     if not link_id:
         return jsonify(Responses.NO_SUCH_SHORT_LINK)
+
     return jsonify({full_link: link_id})
 
 
 def main():
     app.run()
+
 
 
 if __name__ == '__main__':
