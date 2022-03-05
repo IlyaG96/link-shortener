4929497ea24e13bdc675138d505db6a7bc31640a
IlyaG96 <ilyag_96@icloud.com>
2022-03-05 19:02:57 +1100

    fix tests

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,8 +1,11 @@
 import time
-
+import random
+from string import ascii_letters
 import pytest
 from app_factory import create_app
 from link_shortener import Responses
+
+TEST_LINK_NAME = ''.join(random.choice(ascii_letters) for i in range(10))
 
 
 @pytest.fixture
@@ -34,17 +37,17 @@ def test_no_link_in_query_params(client):
 
 
 def test_1incorrect_link_in_query_params(client):
-    response = client.get('/api/custom?link=https://googsssale.com&name=ivan')
+    response = client.get(f'/api/custom?link=https://googsssale.com&name={TEST_LINK_NAME}')
     time.sleep(3)
     assert Responses.INCORRECT_LINK == response.json
 
 
 def test_2incorrect_link_in_query_params(client):
-    response = client.get('/api/custom?link=htt://google.com&name=ivan')
+    response = client.get(f'/api/custom?link=htt://google.com&name={TEST_LINK_NAME}')
     assert Responses.INCORRECT_LINK == response.json
 
 
 def test_existed_link_in_query_params(client):
     response = client.get('/api/custom?link=https://google.com')
-    assert Responses.INCORRECT_LINK == response.json
+    assert Responses.WRONG_QUERY_PARAMS == response.json
 
