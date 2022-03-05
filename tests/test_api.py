37e12c8b9cbfb875451ecf726e86d7c13cd3ca9b
IlyaG96 <ilyag_96@icloud.com>
2022-03-05 23:33:45 +1100

    add test to not-existed link

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,9 +3,11 @@ from string import ascii_letters
 import pytest
 from app_factory import create_app
 from link_shortener import Responses
+from hashlib import sha256
 
 TEST_LINK_NAME = ''.join(random.choice(ascii_letters) for letter in range(10))
 TEST_URL = 'https://google.com'  # change this to any existing url
+TEST_SHORT_LINK_NAME = sha256(TEST_URL.encode()).hexdigest()[:8]
 
 
 @pytest.fixture
@@ -56,8 +58,13 @@ def test_add_new_link_to_db_custom(client):
 
 
 def test_get_new_link_from_db_custom(client):
-    response = client.get(TEST_LINK_NAME)
+    response = client.get(f'/{TEST_LINK_NAME}')
     assert '302 FOUND' == response.status
+
+
+def test_get_not_existed_link_db_custom(client):
+    response = client.get(f'/{TEST_LINK_NAME}dsaeqw')
+    assert Responses.NO_SUCH_SHORT_LINK == response.json
 
 
 def test_existed_link_in_query_params(client):
@@ -90,6 +97,16 @@ def test_2_incorrect_link_in_query_params_auto(client):
     assert Responses.INCORRECT_LINK == response.json
 
 
-def test_existed_link_in_query_params_auto(client):
-    response = client.get('/api/custom?link=https://google.com')
-    assert Responses.WRONG_QUERY_PARAMS == response.json
+def test_write_link_db_auto(client):
+    response = client.get(f'/api/make-short?link={TEST_URL}')
+    assert TEST_SHORT_LINK_NAME in response.json['message']
+
+
+def test_get_link_db_auto(client):
+    response = client.get(f'/{TEST_SHORT_LINK_NAME}')
+    assert '302 FOUND' == response.status
+
+
+def test_get_not_existed_link_db_auto(client):
+    response = client.get(f'/{TEST_SHORT_LINK_NAME}llsdd123124fdsar')
+    assert Responses.NO_SUCH_SHORT_LINK == response.json
